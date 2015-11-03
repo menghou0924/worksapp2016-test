6422674d236421733a9a62ee39809843e106ddf5
menghou0924 <menghou0924@yahoo.com.hk>
2015-11-03 17:24:29 +0800

    Initial Commit

---
+++ Works Application 2016 Snake Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Works Application 2016 Snake Game
+//
+//  Created by Emanuel on 3/11/2015.
+//  Copyright © 2015 Raymond Sak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
